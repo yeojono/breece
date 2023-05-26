afe96ccac4e77201d71c936e67be4f635bc25698
jono <yeojon@outlook.com>
2023-05-26 20:08:21 +1000

    add hard trials

--- src/data/puzzles.ts
+++ src/data/puzzles.ts
@@ -36,24 +36,30 @@ export const practicePuzzles: PuzzleConfig[] = [
 ];
 
 export const puzzles: PuzzleConfig[] = [
-	{ puzzleId: 'easy2', sequence: 'ABABABABA', a: 'star', b: 'triangle' },
-	{ puzzleId: 'random1', sequence: 'ABAABBBAA', a: 'octagon', b: 'square' },
-	{ puzzleId: 'medium1', sequence: 'AABAABAAB', a: 'circle', b: 'star' },
-	{ puzzleId: 'random3', sequence: 'AABABAAAB', a: 'cross', b: 'crescent' },
-	{ puzzleId: 'medium2', sequence: 'AABAABAAB', a: 'plus', b: 'triangle' },
-	{ puzzleId: 'medium6', sequence: 'ABBABBABB', a: 'square', b: 'pentagon' },
 	{ puzzleId: 'easy1', sequence: 'ABABABABA', a: 'square', b: 'circle' },
+	{ puzzleId: 'random4', sequence: 'AABABAAAB', a: 'crescent', b: 'circle' },
+	{ puzzleId: 'medium1', sequence: 'AABAABAAB', a: 'circle', b: 'star' },
 	{ puzzleId: 'random2', sequence: 'ABAABBBAA', a: 'pentagon', b: 'triangle' },
-	{ puzzleId: 'random6', sequence: 'ABBABAABA', a: 'cross', b: 'star' },
 	{ puzzleId: 'easy4', sequence: 'ABABABABA', a: 'star', b: 'square' },
-	{ puzzleId: 'medium5', sequence: 'ABBABBABB', a: 'crescent', b: 'plus' },
 	{ puzzleId: 'easy3', sequence: 'ABABABABA', a: 'plus', b: 'octagon' },
 	{ puzzleId: 'random5', sequence: 'ABBABAABA', a: 'star', b: 'circle' },
 	{ puzzleId: 'medium4', sequence: 'ABAABAABA', a: 'cross', b: 'octagon' },
-	{ puzzleId: 'easy5', sequence: 'ABABABABA', a: 'plus', b: 'circle' },
 	{ puzzleId: 'easy6', sequence: 'ABABABABA', a: 'triangle', b: 'octagon' },
-	{ puzzleId: 'random4', sequence: 'AABABAAAB', a: 'crescent', b: 'circle' },
-	{ puzzleId: 'medium3', sequence: 'ABAABAABA', a: 'pentagon', b: 'square' }
+	{ puzzleId: 'easy2', sequence: 'ABABABABA', a: 'star', b: 'triangle' },
+	{ puzzleId: 'medium2', sequence: 'AABAABAAB', a: 'plus', b: 'triangle' },
+	{ puzzleId: 'hard3', sequence: 'ABAAABAAA', a: 'circle', b: 'crescent' },
+	{ puzzleId: 'medium5', sequence: 'ABBABBABB', a: 'crescent', b: 'plus' },
+	{ puzzleId: 'hard6', sequence: 'ABBAABBAA', a: 'triangle', b: 'circle' },
+	{ puzzleId: 'random1', sequence: 'ABAABBBAA', a: 'octagon', b: 'square' },
+	{ puzzleId: 'hard5', sequence: 'ABBAABBAA', a: 'crescent', b: 'cross' },
+	{ puzzleId: 'easy5', sequence: 'ABABABABA', a: 'plus', b: 'circle' },
+	{ puzzleId: 'hard2', sequence: 'AABAAABAA', a: 'cross', b: 'plus' },
+	{ puzzleId: 'hard4', sequence: 'ABAAABAAA', a: 'pentagon', b: 'star' },
+	{ puzzleId: 'random6', sequence: 'ABBABAABA', a: 'cross', b: 'star' },
+	{ puzzleId: 'medium6', sequence: 'ABBABBABB', a: 'square', b: 'pentagon' },
+	{ puzzleId: 'hard1', sequence: 'AABAAABAA', a: 'triangle', b: 'octagon' },
+	{ puzzleId: 'medium3', sequence: 'ABAABAABA', a: 'pentagon', b: 'square' },
+	{ puzzleId: 'random3', sequence: 'AABABAAAB', a: 'cross', b: 'crescent' },
 ];
 
 export const shapesFromCharacterSequence = (sequence: string, a: ShapeType, b: ShapeType) => {
